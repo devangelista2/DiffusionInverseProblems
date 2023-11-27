d7a3bf2fc0b3e6c4dd2aa7ef2292711f6bcc13af
devangelista2 <davide.evangelista5@unibo.it>
2023-11-27 09:18:37 +0100

    Added Results folder

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from models.DDIM import DDIM
 from models.nn import models
 
 # SET PARAMETERS
-CONFIG_PATH = "./configs/CIFAR10.yml"
-TRAIN = True
+CONFIG_PATH = "./configs/MNIST.yml"
+TRAIN = False
 
 SAVE_RESULTS = True
 
@@ -32,10 +32,27 @@ else:
     weights_path = f"./model_weights/{config.data.dataset}_{config.training.loss}.pth"
     ddim_model.model.load_state_dict(torch.load(weights_path))
 
-# Generate 6 images and visualize them
-x_T = torch.randn((6, 1, 28, 28)).to(config.device)
+# Generate 9 images and visualize them
+x_T = torch.randn(
+    (9, config.data.channels, config.data.image_size, config.data.image_size)
+).to(config.device)
 x_0 = ddim_model.reverse_diffusion(x_T, diffusion_steps=20)
+
+# Results
+#### Create results folder if required
+if not os.path.exists(f"./results/{config.data.dataset}_{config.training.loss}"):
+    os.makedirs(f"./results/{config.data.dataset}_{config.training.loss}")
 
 # Save image if required
 if SAVE_RESULTS:
-    plt.imsave("gen.png", x_0.cpu().detach().numpy()[0, 0], cmap="gray")
+    plt.figure()
+    for i in range(9):
+        plt.subplot(3, 3, i + 1)
+        plt.imshow(x_0.cpu().detach().numpy()[i, 0], cmap="gray")
+        plt.axis("off")
+    plt.tight_layout()
+    plt.savefig(
+        f"./results/{config.data.dataset}_{config.training.loss}/generation.png",
+        dpi=400,
+    )
+    plt.close()
